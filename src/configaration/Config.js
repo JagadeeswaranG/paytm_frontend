7991a9c8ea74f617ad8bd48da9686473e0470202
JagadeeswaranG <jagadees.vg@gmail.com>
2023-03-04 21:43:07 +0530

    render

--- src/configaration/Config.js
+++ src/configaration/Config.js
@@ -2,6 +2,6 @@
 // Back end connection and configaration
 
 export const config = {
-    api: "http://localhost:3000", //"http://localhost:3000"
+    api: "https://paytm-clone-backend.onrender.com", //"http://localhost:3000"
     storage_key: "appoint_key"
 }
